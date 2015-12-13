f9d48961e07dc7211206ee2fca8fb5e025509dc2
Catalin Teodorescu <catalin@9pixels.co>
2015-12-14 00:27:23 +0200

    Support body background effect

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var moveBg = function(el) {
 	var wHeight = $(window).height();  // Window height
 	var elTop = $(el).offset().top;  // Element's top position in page ( Distance from top of page to element's top line ) 
 	var elHeight = $(el).innerHeight();  // Element height
-	var elReachBottom = elTop - wHeight; // Position of scroll when element appear in page from bottom
+	if(elTop > wHeight) {
+		var elReachBottom = elTop - wHeight; // Position of scroll when element appear in page from bottom
+	} else {
+		var elReachBottom = 0;
+	}
 	var elReachTop = elTop + elHeight;  // Element's bottom position in page ( Distance from top of the page to element's bottom line )
 	var i = 20; // Decrease scroll by variable
 
