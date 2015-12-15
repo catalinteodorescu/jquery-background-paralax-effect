4f9fdd6b40bedc7fbbef1135ccb42415da805a80
Catalin Teodorescu <catalin@9pixels.co>
2015-12-15 15:24:50 +0200

    Just include .js file and call the function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Paralax effect - Background moves down when scrolling
-var moveBg = function(el) {
+var moveBg = function(el,i) {
 	var wHeight = $(window).height();  // Window height
 	var elTop = $(el).offset().top;  // Element's top position in page ( Distance from top of page to element's top line ) 
 	var elHeight = $(el).innerHeight();  // Element height
@@ -9,7 +9,6 @@ var moveBg = function(el) {
 		var elReachBottom = 0;
 	}
 	var elReachTop = elTop + elHeight;  // Element's bottom position in page ( Distance from top of the page to element's bottom line )
-	var i = 20; // Decrease scroll by variable
 
 	$(window).scroll(function() {
 		var wScroll = $(this).scrollTop();  // Window top scroll position
@@ -18,5 +17,16 @@ var moveBg = function(el) {
 			$(el).css({"background-position-y":moveDistance, "background-attachment":"fixed"});
 		}
 	});
-}; 
-moveBg(".class");
+};
+
+var initParalaxBg = function(x) {
+
+	$(".paralaxbg").each(function() {
+		var x = $(this).attr("data-paralaxbg-speed");
+		if(x) {
+			moveBg(this,x);
+		} else {
+			moveBg(this,20);
+		}	
+	});
+}
